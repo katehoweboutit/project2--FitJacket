7a3a826ef6dabc57464dde463d3918970531385c
Cashmin <Cashmin2b@gmail.com>
2025-04-03 23:54:20 -0400

    Creating FitUser

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import FitUser
 
 # Register your models here.
+class FitUserAdmin(UserAdmin):
+    model = FitUser
+    list_display = ['email', 'first_name', 'last_name', 'is_staff', 'is_active', 'age', 'weight']
+    list_filter = ['is_staff', 'is_active']
+    search_fields = ['email', 'first_name', 'last_name', 'age', 'weight']
+    ordering = ['email']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'age', 'weight', 'LifestyleHabits', 'AdditionalNotes')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'first_name', 'last_name', 'is_active', 'is_staff')}
+        ),
+    )
+
+admin.site.register(FitUser, FitUserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,38 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 # Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class FitUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)  # Required for admin access
+    date_joined = models.DateTimeField(auto_now_add=True)
+    age = models.PositiveIntegerField(default=20)
+    weight = models.PositiveIntegerField(default=180)
+    LifestyleHabits = models.TextField(blank=True)
+    AdditionalNotes = models.TextField(blank=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']  # Specify fields required for createsuperuser
+
+    def __str__(self):
+        return self.email

d97ae431d67e290a1b392c8263a0939ac04fa265
wangsop <sophia.riverfront@gmail.com>
2025-04-08 12:14:55 -0400

    Created add friend screen, with search for friend functionality (by either first or last name). Also set up bare page for friends list when it is eventually implemented

--- friends/urls.py
+++ friends/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='friends.index')
+    path('', views.index, name='friends.index'),
+    path('/add_friend', views.add_friend, name='friends.add_friend')
 ]

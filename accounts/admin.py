47c59cd9fcfc246d7ec1c83a4ebd21a1c07fcf30
Cashmin <Cashmin2b@gmail.com>
2025-04-09 22:57:39 -0400

    Creating FitUser Signup Page

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import FitUser
+from .forms import FitUserCreationForm #new
+from django.contrib.auth.forms import UserChangeForm #new
 
 # Register your models here.
 class FitUserAdmin(UserAdmin):
+    add_form = FitUserCreationForm #new
+    form = UserChangeForm #new
     model = FitUser
     list_display = ['email', 'first_name', 'last_name', 'is_staff', 'is_active', 'age', 'weight']
     list_filter = ['is_staff', 'is_active']

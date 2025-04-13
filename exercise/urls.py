6d63a689dc4265a7661de49817b471981f13516a
Jhicks77 <jhicks77@gatech.edu>
2025-04-13 02:54:14 -0400

    Add placeholder for add exercise screen

--- exercise/urls.py
+++ exercise/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='exercise.index'),
-    path('', views.index, name='exercise.show')
+    path('newExercise', views.new_exercise, name='exercise.new_exercise')
 ]
--- exercise/views.py
+++ exercise/views.py
@@ -6,3 +6,10 @@ def index(request):
     template_data['title'] = 'Exercises'
     return render(request, 'exercise/index.html',
                   {'template_data': template_data})
+
+
+def new_exercise(request):
+    template_data = {}
+    template_data['title'] = 'Exercises'
+    return render(request, 'exercise/new_exercise.html',
+                  {'template_data': template_data})

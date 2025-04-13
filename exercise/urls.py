145a9fc2ee505a3c4562539787c2ba858ebdc8c9
Jhicks77 <jhicks77@gatech.edu>
2025-04-13 08:12:29 -0400

    Add viewExercise view and dynamic buttons to view exercises

--- exercise/urls.py
+++ exercise/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='exercise.index'),
-    path('newExercise/', views.new_exercise, name='exercise.new_exercise')
+    path('newExercise/', views.new_exercise, name='exercise.new_exercise'),
+    path('viewExercise/<int:id>', views.view_exercise, name='exercise.viewExercise')
 ]
--- exercise/views.py
+++ exercise/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .utils import all_muscle_groups, update_db_exercises, update_assigned_exercises
+from .models import ExerciseAssignment
 
 def index(request):
     template_data = {}
     template_data['title'] = 'Exercises'
+    template_data['exercise_assignments'] = ExerciseAssignment.objects.filter(user=request.user)
     return render(request, 'exercise/index.html',
                   {'template_data': template_data})
 
@@ -15,10 +17,19 @@ def new_exercise(request):
         update_db_exercises(checked_muscle_groups)
 
         update_assigned_exercises(request.user, checked_muscle_groups)
+        return redirect('exercise.index')
 
     template_data = {}
     template_data['title'] = 'New Exercise'
     template_data['muscle_groups'] = all_muscle_groups
 
     return render(request, 'exercise/new_exercise.html',
+                  {'template_data': template_data})
+
+def view_exercise(request, id):
+    template_data = {}
+    template_data['title'] = 'View Exercise'
+    template_data['exercise_id'] = id
+
+    return render(request, 'exercise/viewExercise.html',
                   {'template_data': template_data})

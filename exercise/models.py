7e4ec2ec45c476fc3076ca15d1210eabb9fd5a1b
Jhicks77 <jhicks77@gatech.edu>
2025-04-13 02:21:27 -0400

    Add Exercise and ExerciseAssignment models

--- exercise/models.py
+++ exercise/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from accounts.models import FitUser
 
-# Create your models here.
+class Exercise(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=128)
+    equipment_needed = models.CharField(max_length=128)
+    instructions = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+
+class ExerciseAssignment(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(FitUser, on_delete=models.CASCADE)
+    completed = models.BooleanField(default=False)

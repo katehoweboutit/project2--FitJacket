730dd71a586e700db4db4f0e63372929e5574a1e
Cashmin <Cashmin2b@gmail.com>
2025-04-20 16:52:12 -0400

    Adding Password Reset Functionality

--- accounts/views.py
+++ accounts/views.py
@@ -3,12 +3,16 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login as auth_login, authenticate, logout as auth_logout
 from .forms import FitUserCreationForm
+from django.contrib.auth import get_user_model
 #
 from .forms import FitUserCreationForm
 #
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 
 
 # Create your views here.
@@ -82,3 +86,33 @@ def orders(request):
 
     return render(request, 'accounts/orders.html',
                   {'template_data': template_data})
+
+def reset_password_username(request):
+    template_data = {'title': 'Reset Password'}
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        new_password = request.POST.get('new_password')
+        confirm_password = request.POST.get('confirm_password')
+
+        if new_password != confirm_password:
+            messages.error(request, "Passwords do not match.")
+            return render(request, 'accounts/reset_password_username.html', {'template_data': template_data})
+        FitModel = get_user_model()
+        try:
+            validate_password(new_password)
+        except ValidationError as invalid:
+            for msg in invalid.messages:
+                messages.error(request, msg)
+            return render(request, 'accounts/reset_password_username.html', {'template_data': template_data})
+        try:
+            user = FitModel.objects.get(username=username)
+            user.set_password(new_password)
+            user.save()
+            messages.success(request, "Password has been reset successfully. You can now log in.")
+            list(messages.get_messages(request))
+            return redirect('accounts.login')
+        except FitModel.DoesNotExist:
+            messages.error(request, "User not found.")
+
+    return render(request, 'accounts/reset_password_username.html', {'template_data': template_data})

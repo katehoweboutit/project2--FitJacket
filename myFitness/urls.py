8a021d15b5c9b0d6a23a16ead4256cd10a3c1d7a
katehoweboutit <klindley8@gatech.edu>
2025-04-23 22:08:41 -0400

    Hey, I'm super confused, lmk if youre the goat by helping me out, best wishes

--- myFitness/urls.py
+++ myFitness/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='myFitness.index')
+    path('', views.index, name='myFitness.index'),
+    path('myFitness.listofDeath', views.index, name='myFitness.listofDeath'),
+    path('myFitness.randoList', views.index, name='myFitness.randoList'),
 ]

fe24915c4913a3d64e77c9d455f79b83181c7b89
Jhicks77 <jhicks77@gatech.edu>
2025-04-26 17:24:10 -0400

    Remove several placeholder/redundant views, comments, models, urls, and files

--- myFitness/views.py
+++ myFitness/views.py
@@ -1,35 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import createMyFitness
 from .utils import get_user_activity_by_muscle
+
 def index(request):
-    template_data = {}
-    template_data['title'] = 'My Fitness'
-    return render(request, 'myFitness/index.html',
-                  {'template_data': template_data})
-def data(request, id):
-    userActivity = get_user_activity_by_muscle(id)
-    if userActivity is None:
+    userActivity = get_user_activity_by_muscle(request.user)
+
+    if sum(userActivity.values()) == 0: # if the user has not completed any exercises
         return render(request, 'myFitness/error.html')
     else:
-        return render(request, 'myFitness/data.html',
-                      {'userActivity': userActivity})
-#request.user.fit_points += exercise.fitpoint_reward
-# def listofDeath(request, id):
-#     #exercise = ExerciseAssignment.objects.get(id=id)
-#     template_data = createMyFitness(id).calculate_fitnessing()
-#     return render(request, 'myFitness/index.html', {'template_data': template_data})
-#     #exercise.completed = True
-#     #exercise.save()
-#     #request.user.fit_points += exercise.fitpoint_reward
-#     #request.user.save()
-#
-#     #template_data = {}
-#     #template_data['title'] = 'Complete Exercise'
-#     #template_data['exercise'] = exercise
-#     #template_data['user_name'] = request.user.first_name
-#
-#     #return render(request, 'exercise/complete_exercise.html', {'template_data': template_data})
-# def randoList(request, id):
-#     template_data = createMyFitness(id).rando_fitness()
-#     return render(request, 'myFitness/index.html', {'template_data': template_data})
+        return render(request, 'myFitness/index.html',
+                  {'userActivity': userActivity})

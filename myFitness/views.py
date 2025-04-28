15eb39ac7cc01482da66ea5125257d5aacc602a7
Jhicks77 <jhicks77@gatech.edu>
2025-04-27 22:24:14 -0400

    Add My Fitness tab title to my fitness page

--- myFitness/views.py
+++ myFitness/views.py
@@ -4,8 +4,13 @@ from .utils import get_user_activity_by_muscle
 def index(request):
     userActivity = get_user_activity_by_muscle(request.user)
 
+    template_data = {}
+    template_data['userActivity'] = userActivity
+    template_data['title'] = 'My Fitness'
+
     if sum(userActivity.values()) == 0: # if the user has not completed any exercises
-        return render(request, 'myFitness/error.html')
+        return render(request, 'myFitness/error.html',
+                      {'template_data': template_data})
     else:
         return render(request, 'myFitness/index.html',
-                  {'userActivity': userActivity})
+                  {'template_data': template_data})
